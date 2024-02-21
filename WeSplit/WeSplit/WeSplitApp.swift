8de078cdb2a559aa641dad6f93b7725d6f8ec09f
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2024-02-20 20:48:19 -0800

    Initial commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Immanuel Matthews-Feemster on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
