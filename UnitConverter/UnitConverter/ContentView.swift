4ed5ab32ec656cfcd45a29a4f61f05bf19b67d74
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2024-03-06 06:45:45 -0800

    Main picker OnChange functionality

--- UnitConverter/UnitConverter/ContentView.swift
+++ UnitConverter/UnitConverter/ContentView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedUnitType = "Temperature"
+    @State private var selectedUnitType = "Temp"
+    @State private var convertFromUnit = "Fahrenheit"
+    @State private var convertToUnit = "Celcius"
     
     private let unitType = ["Temp", "Length", "Time", "Volume"]
+    private let tempUnits = ["Fahrenheit", "Celcius", "Kelvin"]
+    private let lengthUnits = ["Meters", "Kilometers", "Feet", "Yards", "Miles"]
+    private let timeUnits = ["Seconds", "Minutes", "Hours", "Days"]
+    private let volumeUnits = ["Milliters", "Liters", "Cups", "Pints", "Gallons"]
+    
+    @State private var conversionUnits = ["Fahrenheit", "Celcius", "Kelvin"]
+    
     
     var body: some View {
         NavigationView {
@@ -22,14 +31,46 @@ struct ContentView: View {
                         }
                     }
                     .pickerStyle(.segmented)
+                    .onChange(of: selectedUnitType) {
+                        updateUnits(selectedUnitType)
+                        
+                        
+                    }
                 }
+                Section {
+                    Picker("Conversion Units", selection: $conversionUnits[0]) {
+                        ForEach(conversionUnits, id: \.self) { type in
+                            Text(type).tag(type)
+                        }
+                    }
+                    Picker("Conversion Units", selection: $conversionUnits[1]) {
+                        ForEach(conversionUnits, id: \.self) { type in
+                            Text(type).tag(type)
+                        }
+                    }
+
+                }
+
             }
             .navigationTitle("Unit Converter")
         }
     }
     
+    func updateUnits(_ unitIndex: String) {
+        switch unitIndex {
+        case "Temp":
+            conversionUnits = tempUnits
+        case "Length":
+            conversionUnits = lengthUnits
+        case "Time":
+            conversionUnits = timeUnits
+        case "Volume":
+            conversionUnits = volumeUnits
+        default :
+            conversionUnits = ["updateUnitsError","switchFunctionError"]
+        }
+    }
 }
-
 #Preview {
     ContentView()
 }
