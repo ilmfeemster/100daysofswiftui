334d3219c520b4a76c506c6962baee1415f4d2e8
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2024-03-10 10:44:46 -0700

    Day 21

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Immanuel Matthews-Feemster on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
