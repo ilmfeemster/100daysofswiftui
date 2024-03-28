2fc72aa85e17aefe3e11fa60db3454e5a51a5ca4
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2024-03-28 16:33:36 -0700

    Day 27

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Immanuel Matthews-Feemster on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
