1acf2ce5a6414657a7b2ea4b9d01a1049d4b3cd9
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2024-03-29 05:57:33 -0700

    Day 29

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Immanuel Matthews-Feemster on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
