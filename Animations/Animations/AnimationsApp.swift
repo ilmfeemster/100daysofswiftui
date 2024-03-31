a3e7aee1110772cffcaee976a93416da08a0f14f
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2024-03-31 15:08:34 -0700

    Day 32

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Immanuel Matthews-Feemster on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
