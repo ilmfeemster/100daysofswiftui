112d16eb2b790444f7645d8757f64d4d0e74b690
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2024-05-15 06:57:13 -0700

    Day 37 & 38

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,15 +7,87 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
         }
-        .padding()
+    }
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+
+        items = []
+    }
+}
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
+    private let expenseTypes = ["Personal", "Business"]
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenseTypes, id: \.self) {type in
+                    Section(header: Text(type)) {
+                        ForEach(expenses.items.filter{$0.type == type}) { item in
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(item.name)
+                                        .font(.headline)
+                                        .foregroundStyle(expenseStyling(amount: item.amount))
+                                    
+                                    Text(item.type)
+                                }
+                                
+                                Spacer()
+                                
+                                Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            }
+                        }
+                        .onDelete(perform: removeItems)
+                    }
+                }
+            }
+            .navigationTitle("iExpense")
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    showingAddExpense = true
+                }
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+    
+    func expenseStyling(amount: Double) -> any ShapeStyle{
+        if amount < 10 {
+            return .blue
+        } else if amount < 100 {
+            return .green
+        } else {
+            return .red
+        }
     }
 }
 
