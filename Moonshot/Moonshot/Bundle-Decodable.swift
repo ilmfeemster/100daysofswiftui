5f1931dbd3b01383b246ddf77062f5526af20ac9
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2024-06-17 19:42:12 -0700

    Day 40 part 2

--- Moonshot/Moonshot/Bundle-Decodable.swift
+++ Moonshot/Moonshot/Bundle-Decodable.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Bundle {
-    func decode<T: Codable>(_ file: String) -> T {
+    func decode<T: Decodable>(_ file: String) -> T{
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle.")
         }
